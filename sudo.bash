2f2f965ec6b73da2a8da2302c825f682f6f7108d
Stanislav Seletskiy <s.seletskiy@gmail.com>
2016-05-26 16:29:41 +0600

    debug to stderr

--- sudo.bash
+++ sudo.bash
@@ -6,16 +6,16 @@ function sudo() {
         printf "\e[1;31m       .  %s\e[0m\n" "${@:2}"
     } | :sudo:indent
 
-    :sudo:silent "${@}"
+    sudo:silent "${@}"
 }
 
 function sudo:silent() {
-    command sudo ${_sudo_default_options[@]} "${@}"}
+    command sudo ${_sudo_default_options[@]} "${@}"
 }
 
 :sudo:indent() {
     sed -ur \
         -e 's/^/    /' \
         -e '1i\ ' \
-        -e '$a\ '
+        -e '$a\ ' >&2
 }
